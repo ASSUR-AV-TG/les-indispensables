62376772ee635c074713a6c4e3ae424ca8f02a9e
MAMAH Abdou-Salami <assuravenirtogo@gmail.com>
2025-10-18 19:39:04 +0000

    Ajout du site complet avec CSS, JS et image logo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-console.log("Bienvenue sur LES INDISPENSABLES !");
+console.log("JavaScript fonctionne !");
+
+document.getElementById("btn-test").addEventListener("click", function() {
+    alert("Bravo ! Le JS fonctionne aussi sur le site.");
+});
